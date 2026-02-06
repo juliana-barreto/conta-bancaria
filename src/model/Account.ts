eadf0e9415133e37cd7c44d015c168c3b5ea7e6e
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-06 18:22:05 -0300

    :recycle: Refatoração do código para inglês

---
+++ src/model/Account.ts
@@ -0,0 +1,138 @@
+import { Colors } from "../util/Colors";
+
+export abstract class Account {
+
+  // Atributos
+  private _number: number;
+  private _branch: number;
+  private _holder: string;
+  private _type: number;
+  private _balance: number;
+
+  // Construtor
+  constructor(number: number, branch: number, holder: string, type: number, balance: number) {
+    this._number = number;
+    this._branch = branch;
+    this._holder = holder;
+    this._type = type;
+    this._balance = balance;
+  }
+
+  // Getters
+
+	public get number(): number {
+		return this._number;
+	}
+
+	public get branch(): number {
+		return this._branch;
+	}
+
+	public get holder(): string {
+		return this._holder;
+	}
+
+	public get type(): number {
+		return this._type;
+	}
+
+	public get balance(): number {
+		return this._balance;
+	}
+
+  // Setters com validações
+
+	public set number(value: number) {
+    if (value <= 0) {
+      console.log(Colors.fg.red, "O número da conta deve ser positivo!", Colors.reset);
+      return;
+    }
+		this._number = value;
+	}
+
+	public set branch(value: number) {
+    if (value <= 0) {
+      console.log(Colors.fg.red, "O número da agência deve ser positivo!", Colors.reset);
+      return;
+    }
+		this._branch = value;
+	}
+
+	public set holder(value: string) {
+    if (value === "") {
+      console.log(Colors.fg.red, "O nome do titular não pode ser vazio!", Colors.reset);
+      return;
+    }
+		this._holder = value;
+	}
+
+	public set type(value: number) {
+    if (type !== 1 && type !== 2) {
+      console.log(Colors.fg.red, "O tipo da conta deve ser 1 ou 2!", Colors.reset);
+      return;
+    }
+		this._type = value;
+	}
+
+	public set balance(value: number) {
+    if (value < 0) {
+      console.log(Colors.fg.red, "O saldo não pode ser negativo!", Colors.reset);
+      return;
+    }
+		this._balance = value;
+	}
+
+//Métodos Auxiliares
+
+public withdraw(valor: number): boolean{
+
+    if(valor <= 0){
+        console.log(Colors.fg.red, "O valor deve ser positivo!", Colors.reset);
+        return false;
+    }
+
+    if (valor > this._balance) {
+        console.log(Colors.fg.red, "Saldo insuficiente!", Colors.reset);
+        return false;
+    }
+    
+    this._balance -= valor;
+    return true;
+}
+
+public deposit(valor: number): void{
+    if (valor <= 0) {
+        console.log(Colors.fg.red, "Depósito inválido!", Colors.reset);
+    }
+
+    this._balance += valor;
+}
+
+public view(): void{
+
+    let tipo: string;
+
+    switch (this._type) {
+        case 1:
+            tipo = "Conta Corrente";
+            break;
+        case 2:
+            tipo = "Conta Poupança";
+            break;
+    
+        default:
+            tipo = "Tipo inválido!";
+            break;
+    }
+
+    console.log("\n*****************************************************");
+    console.log("                    DADOS DA CONTA                     ");
+    console.log("*****************************************************");
+    console.log(`Número da conta: ${this._number}`);
+    console.log(`Número da agência: ${this._branch}`);
+    console.log(`Nome do titular: ${this._holder}`);
+    console.log(`Tipo da conta: ${this._type}`);
+    console.log(`Saldo: R$${this._balance.toFixed(2)}`);
+    console.log("*****************************************************\n");
+  }
+}
