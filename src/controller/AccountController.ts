3b43db5d6d4d9b421fec5d963a0715a374f37706
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-10 11:10:58 -0300

    ✨ Implementação da classe AccountController com métodos CRUD e bancários

---
+++ src/controller/AccountController.ts
@@ -0,0 +1,127 @@
+import { Account } from '../model/Account';
+import { AccountRepository } from '../repository/AccountRepository';
+import { Colors } from '../util/Colors';
+import { formatCurrency } from '../util/Currency';
+import { Input } from '../util/Input';
+
+export class AccountController implements AccountRepository{
+
+    private accounts = new Array<Account>();
+
+    private nextNumber: number = 0;
+
+    //Métodos do CRUD
+    findByNumber(number: number): void {
+        const foundAccount = this.findInArray(number);
+
+        if(foundAccount !== null)
+            foundAccount.view();
+        else
+            console.log(Colors.fg.red, '\nConta não encontrada!', Colors.reset);
+    }
+
+    listAll(): void {
+        for (let account of this.accounts) {
+            account.view();
+        }
+    }
+
+    findByHolder(holder: string): void{
+        const accountsByHolder = this.accounts.filter( account => account.holder.toLocaleUpperCase().includes(holder.toUpperCase()));
+
+        if (accountsByHolder.length > 0) {
+            accountsByHolder.forEach(account => account.view());
+        } else {
+            console.log('Nenhuma conta foi encontrada!');
+        }
+    }
+
+    create(account: Account): void {
+        this.accounts.push(account);
+        console.log(`\nA Conta número ${account.number} foi cadastrada com sucesso!`);
+    }
+
+    update(account: Account): void {
+        const foundAccount = this.findInArray(account.number);
+
+        if(foundAccount !== null){
+            this.accounts[this.accounts.indexOf(foundAccount)] = account;
+            console.log(`\nA Conta número ${account.number} foi atualizada com Sucesso!`);
+        }
+        else {
+            console.log(`\nConta não encontrada!`);
+        }
+    }
+
+    delete(number: number): void {
+        const foundAccount = this.findInArray(number);
+
+        if(foundAccount !== null){
+            
+            let confirmDeletion: string;
+            console.log('Para confirmar deleção, digite o nome completo do titular:', foundAccount.holder);
+            confirmDeletion = Input.question('');
+            if (confirmDeletion.trim().toLowerCase() === foundAccount.holder.trim().toLowerCase()) {
+            this.accounts.splice(this.accounts.indexOf(foundAccount), 1);
+            console.log(Colors.fg.greenstrong, `\nA Conta número${number} foi deletada com sucesso!`, Colors.reset);
+                
+            }else{
+                console.log('Nome incorreto. Operação cancelada.');
+
+            }
+        }
+        else{
+            console.log(Colors.fg.red, '\nConta não encontrada!', Colors.reset);
+        }
+    }
+
+    //Métodos Bancários
+    withdraw(number: number, amount: number): void {
+        const foundAccount = this.findInArray(number);
+
+        if(foundAccount !== null){
+            if (foundAccount.withdraw(amount) === true) {
+                console.log(`\nO saque no valor de ${formatCurrency(amount)} na Conta número ${number} foi realizada com sucesso!`);
+            }
+        }else{
+            console.log(Colors.fg.red, '\nConta não encontrada!', Colors.reset);}
+    }
+
+    deposit(number: number, amount: number): void {
+        const foundAccount = this.findInArray(number);
+
+        if(foundAccount !== null){
+            foundAccount.deposit(amount)
+                console.log(`\nO depósito no valor de ${formatCurrency(amount)} na Conta número ${number} foi realizada com sucesso!`);
+            
+        }else{
+            console.log(Colors.fg.red, '\nConta não encontrada!', Colors.reset);}
+    }
+
+    transfer(sourceNumber: number, targetNumber: number, amount: number): void {
+        const sourceAccount = this.findInArray(sourceNumber);
+        const targetAccount = this.findInArray(targetNumber);
+        if (sourceAccount !== null && targetAccount !== null) {
+            if (sourceAccount.withdraw(amount) === true) {
+                targetAccount.deposit(amount);
+                console.log(`\nA transferência no valor ${formatCurrency(amount)} da Conta número ${sourceNumber} para a conta número ${targetNumber} foi feita com sucesso!`);
+            }
+        }
+    }
+    
+    //Métodos Auxiliares
+    
+    public generateNumber(): number{
+        return ++ this.nextNumber;
+    }
+
+    public findInArray(number: number): Account | null{
+        for (let account of this.accounts) {
+            if (account.number === number) {
+                return account;
+            }
+            
+        }
+        return null;
+    }
+}
